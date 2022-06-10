3c195c9496a24a34bc7a8f7452991c4c881aea22
Vu Nguyen Ngoc Anh <>
2022-06-10 18:22:33 +0700

    feat(): init project

---
+++ android/app/build/generated/source/buildConfig/debug/com/demo/BuildConfig.java
@@ -0,0 +1,28 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String APP_VERSION = "1.0.0";
+  // Field from default config.
+  public static final String BASE_URL = "https://api.binance.com";
+  // Field from default config.
+  public static final String IOS_APP_ID = ".application.demo";
+  // Field from default config.
+  public static final String IOS_APP_NAME = "Demo001";
+  // Field from default config.
+  public static final String IOS_PROJECT_NAME = "Demo";
+  // Field from default config.
+  public static final String IOS_PROJECT_PATH = "./ios";
+  // Field from default config.
+  public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
+  // Field from default config.
+  public static final String URL_TIME_OUT = "10000";
+}
